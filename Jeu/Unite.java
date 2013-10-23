7630d423f711e44fe986fc2e35585ffbdcf6be1a
Canardout <Canardout@gmail.com>
2013-10-23 18:52:27 +0200

    Create Unite.java

---
+++ Jeu/Unite.java
@@ -0,0 +1,11 @@
+package jeu;
+/** Classe qui définis les Unités , chaque Unité (hors batiment) hérite de cette classe.
+ * 
+ * @author fayej
+ *
+ */
+public class Unite extends ObjectMap {
+	Environnement Env; // Environnement entourant l'unité
+	
+	
+}
