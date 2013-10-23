49b753cfbc480e00a00770832bb5eb482c4a64a5
Canardout <Canardout@gmail.com>
2013-10-23 19:08:16 +0200

    Create Forum.java

---
+++ Jeu/Forum.java
@@ -0,0 +1,25 @@
+package jeu;
+
+/**
+ * Classe "FORUM" définis le batiment Forum et son activité au cours de son activation.
+ * @author fayej
+ *
+ */
+
+
+
+public class Forum extends Batiment {
+	
+	public Forum()  {
+		
+	}
+
+	
+	protected void activate(){
+		// Définir le rôle de l'objet "FORUM" dans la société
+		//requestRole(Société.COMMUNAUTE , Société.GROUPE, Société.ROLE);
+	}
+
+	
+
+}
